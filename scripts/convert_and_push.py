b44d0bcca4dac147ded32392bacdd48533b4db62
Mert Cobanov <mertcobanov@gmail.com>
2024-12-10 22:39:51 +0300

    Update tokenizer to version 50000 and adjust output directory structure

--- scripts/convert_and_push.py
+++ scripts/convert_and_push.py
@@ -1,13 +1,13 @@
 from transformers import PreTrainedTokenizerFast
 import os
 
-tokenizer_path = "tokenizer/turkish_bpe_tokenizer_30000.json"
-output_dir = "tokenizer/hf_tokenizer"
-hub_model_id = "mertcobanov/turkish-bpe-tokenizer-30000"
+tokenizer_path = "tokenizer/turkish_bpe_tokenizer_50000.json"
+output_dir = "tokenizer/"
+hub_model_id = "mertcobanov/turkish-bpe-tokenizer-50000"
 
 tokenizer = PreTrainedTokenizerFast(tokenizer_file=tokenizer_path)
 print("Read from tokenizer file:", tokenizer_path)
-tokenizer.save_pretrained(os.path.join(output_dir, "hf_tokenizer"))
-print("Saved to:", os.path.join(output_dir, "hf_tokenizer"))
+tokenizer.save_pretrained(os.path.join(output_dir, "hf_tokenizer_50000"))
+print("Saved to:", os.path.join(output_dir, "hf_tokenizer_50000"))
 tokenizer.push_to_hub(f"{hub_model_id}")
 print(f"Pushed to Hugging Face https://huggingface.co/{hub_model_id}")
